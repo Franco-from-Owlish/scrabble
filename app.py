0b358b872c2c891c12c74a47249040783b23c2c8
Franco Grobler <franco0927@icloud.com>
2023-03-19 12:43:57 +0200

    Added comments

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
-from flask import Flask, request, json
+from flask import Flask, request, render_template, redirect
 import sqlite3
 import random
 
+# Connect to the DB
 connection = sqlite3.connect("scrabble.db", check_same_thread=False)
+# Create a cursor to query the DB
 cursor = connection.cursor()
+
+# The Flask server
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    Point all pages to the main page.
+    :param e: The error received
+    :return: Redirect to home page.
+    """
+    return redirect('/')
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    """
+    The home page
+    :return: A vue.js app
+    """
+    return render_template('index.html')
 
 
 @app.route('/sentence', methods=['POST'])
 def get_new_sentence():
+    """
+    Endpoint to create the new sentence.
+    :return: JSON object containing the new sentence.
+    """
     sentence = request.form['sentence']
     new_sentence = ''
     if sentence:
@@ -24,10 +46,15 @@ def get_new_sentence():
 
     return {
         'new_sentence': new_sentence.strip().capitalize()
-    }
+    }, 201
 
 
 def replace_word(old_word: str):
+    """
+    Replaces a word with one of the same length starting with the same character.
+    :param old_word: The word to be replaced
+    :return: str
+    """
     old_word = old_word.lower()
     sql = "SELECT word FROM words " \
           "WHERE words.first_letter = ? " \
@@ -39,5 +66,6 @@ def replace_word(old_word: str):
     return random.choice(words) if len(words) > 0 else old_word
 
 
+# Run the sever if the file is called from the command line.
 if __name__ == '__main__':
     app.run()
